2e770f5318fda556fa81db94c843a11ba48d1285
Will Maier <willmaier@ml1.net>
2010-11-17 16:52:52 -0600

    Hack in a query implementation.

--- lib/puppet/provider/package/openbsd.rb
+++ lib/puppet/provider/package/openbsd.rb
@@ -5,6 +5,7 @@ Puppet::Type.type(:package).provide :openbsd, :parent => Puppet::Provider::Packa
   desc "OpenBSD's form of `pkg_add` support."
 
   commands :pkginfo => "pkg_info", :pkgadd => "pkg_add", :pkgdelete => "pkg_delete"
+  commands :perl => "perl"
   pkghelper = File::join(File::dirname(__FILE__), "pkghelper.pl")
 
   defaultfor :operatingsystem => :openbsd
@@ -12,4 +13,26 @@ Puppet::Type.type(:package).provide :openbsd, :parent => Puppet::Provider::Packa
 
   has_feature :versionable
 
+  def query
+    packages = []
+    begin
+      output = perl(:pkginfo, "-e", @resource[:name])
+    rescue Puppet::ExecutionFailure
+       return nil
+    end
+
+    output.each { |line|
+      line.chomp!
+      fields = [:stem, :version, :flavor]
+      if match = %r{^inst:(.*)-(\d.*?)(-.*)?$}.match(line)
+        hash = {}
+        fields.zip(match.captures) { | field, value|
+            hash[field] = value
+        }
+        packages << new(hash)
+      end
+    }
+
+    packages
+  end
 end
