6eca2d4e1c2dee283a558989436da7ac4272d007
Will Maier <willmaier@ml1.net>
2010-11-17 17:23:47 -0600

    Tweak.

--- lib/puppet/provider/package/openbsd.rb
+++ lib/puppet/provider/package/openbsd.rb
@@ -21,32 +21,34 @@ Puppet::Type.type(:package).provide :openbsd, :parent => Puppet::Provider::Packa
 
   def self.instances
     packages = []
-    do_pkginfo(@resource[:name]).each do |pkg|
+    do_pkginfo().each do |pkg|
+        pkg[:ensure] = :installed
         packages << new(pkg)
     end
     packages
   end
 
-  def do_pkginfo(pkgspec)
-    packages = []
-    begin
-      output = pkginfo("-e", pkgspec)
-    rescue Puppet::ExecutionFailure
-       return nil
-    end
+end
 
-    output.each { |line|
-      line.chomp!
-      fields = [:stem, :version, :flavor]
-      if match = %r{^inst:(.*)-(\d.*?)(-.*)?$}.match(line)
-        hash = {}
-        fields.zip(match.captures) { | field, value|
-            hash[field] = value
-        }
-        packages << hash
-      end
-    }
-
-    packages
+def do_pkginfo(pkgspec = "*-*")
+  packages = []
+  begin
+    output = pkginfo("-e", pkgspec)
+  rescue Puppet::ExecutionFailure
+     return nil
   end
+
+  output.each { |line|
+    line.chomp!
+    fields = [:name, :version, :flavor]
+    if match = %r{^inst:(.*)-(\d.*?)(-.*)?$}.match(line)
+      hash = {}
+      fields.zip(match.captures) { | field, value|
+          hash[field] = value
+      }
+      packages << hash
+    end
+  }
+
+  packages
 end
