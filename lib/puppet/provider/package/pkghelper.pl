95cea4173d25599c1901cbed39f4f124ac15d387
Will Maier <willmaier@ml1.net>
2010-11-18 18:10:35 -0600

    Add comments to pkghelper.pl.

--- lib/puppet/provider/package/pkghelper.pl
+++ lib/puppet/provider/package/pkghelper.pl
@@ -3,14 +3,18 @@ use OpenBSD::Search;
 
 my $state = OpenBSD::PkgInfo::State->new("pkg_helper");
 for my $pattern (@ARGV) {
-	my $spec = OpenBSD::Search::PkgSpec->new($pattern);
-	my $r = $state->repo->match_locations($spec);
+    my $spec = OpenBSD::Search::PkgSpec->new($pattern);
+    my $r = $state->repo->match_locations($spec);
 
-	for my $p (@$r) {
-		my $status = "absent";
-		if (OpenBSD::PackageInfo::is_installed($p->name)) {
-			$status = "present";
-		};
-		print $status . "\t" . $pattern . "\t" . $p->name . "\n";
-	}
+    # Search for the package spec (see packages-specs(7) on OpenBSD) in a
+    # package repository. The repository is built from the PKG_PATH environment
+    # variable and may include paths on the network. If the pattern doesn't
+    # match at all, its status is "absent".
+    for my $p (@$r) {
+        my $status = "absent";
+        if (OpenBSD::PackageInfo::is_installed($p->name)) {
+            $status = "present";
+        };
+        print $status . "\t" . $pattern . "\t" . $p->name . "\n";
+    }
 }
